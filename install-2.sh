0b27d2f0ac1fc4261abb938b451ff1eccdefafee
undefinedcode0 <134721497+undefinedcode0@users.noreply.github.com>
2024-01-02 12:16:28 -0600

    Update install-2.sh

--- install-2.sh
+++ install-2.sh
@@ -1,20 +1,47 @@
 #! /bin/bash
 
+# Prompt the user for their desired username and hostname
+read -p "Enter your username: " username
+read -p "Enter your hostname: " hostname
+
+# Set timezone and system clock
 ln -sf /usr/share/zoneinfo/America/Mexico_City /etc/localtime
 hwclock --systohc
+
+# Install necessary packages
 sudo pacman -S nano bash-completion
+
+# Set system locale
 echo LANG=en_US.UTF-8 > /etc/locale.conf
-echo undefinedcode > /etc/hostname
+
+# Set user-defined hostname
+echo "$hostname" > /etc/hostname
+
+# Set password for root
 passwd
-useradd -m -g users -G wheel,storage,power -s /bin/bash undefinedcode
-passwd undefinedcode
+
+# Create user with specified username and add to necessary groups
+useradd -m -g users -G wheel,storage,power -s /bin/bash "$username"
+passwd "$username"
+
+# Configure sudoers file
 EDITOR=nano visudo
+
+# Additional configurations (if needed)
 nano /etc/pacman.conf
+
+# Install bootloader
 bootctl install
-echo title Arch Linux > /boot/loader/entries/arch.conf
-echo linux /vmlinuz-linux >> /boot/loader/entries/arch.conf
-echo initrd /initramfs-linux.img >> /boot/loader/entries/arch.conf
+
+# Configure bootloader entries
+echo "title Arch Linux" > /boot/loader/entries/arch.conf
+echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
+echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
+echo "options root=PARTUUID=$(blkid -s PARTUUID -o value /dev/sda3) rw" >> /boot/loader/entries/arch.conf
+
+# Install and enable NetworkManager
 sudo pacman -S networkmanager
 sudo systemctl enable NetworkManager.service
-echo "options root=PARTUUID=$(blkid -s PARTUUID -o value /dev/sda3) rw" >> /boot/loader/entries/arch.conf
-echo Done! Ctrl-D Run "umount -R /mnt;reboot"
+
+# Completion message
+echo "Done! Press Ctrl-D to exit chroot environment, then run 'umount -R /mnt;reboot'."
