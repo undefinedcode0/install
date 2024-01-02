796a197af702b412297022f88737353c07a45c58
undefinedcode0 <134721497+undefinedcode0@users.noreply.github.com>
2024-01-02 12:02:10 -0600

    Create install-2.sh

---
+++ install-2.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+ln -sf /usr/share/zoneinfo/America/Mexico_City /etc/localtime
+hwclock --systohc
+sudo pacman -S nano bash-completion
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo undefinedcode > /etc/hostname
+passwd
+useradd -m -g users -G wheel,storage,power -s /bin/bash undefinedcode
+EDITOR=nano visudo
+nano /etc/pacman.conf
+bootctl install
+echo title Arch Linux > /boot/loader/entries/arch.conf
+echo linux /vmlinuz-linux >> /boot/loader/entries/arch.conf
+echo initrd /initramfs-linux.img >> /boot/loader/entries/arch.conf
+sudo pacman -S networkmanager
+sudo systemctl enable NetworkManager.service
+echo "options root=PARTUUID=$(blkid -s PARTUUID -o value /dev/sda3) rw" >> /boot/loader/entries/arch.conf
+echo Done! Ctrl-D Run "umount -R /mnt;reboot"
