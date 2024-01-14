6b6531a8876c6fe7cd75a6cc7f4803fe12e62988
undefinedcode0 <134721497+undefinedcode0@users.noreply.github.com>
2024-01-14 10:41:10 -0600

    Update install-2.sh

--- install-2.sh
+++ install-2.sh
@@ -12,7 +12,9 @@ hwclock --systohc
 sudo pacman -S nano bash-completion
 
 # Set system locale
+echo en_US.UTF-8 UTF-8 > /etc/locale.gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
+locale-gen
 
 # Set user-defined hostname
 echo "$hostname" > /etc/hostname
