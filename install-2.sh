2cde1d26e62c65d6b5af7ef843c51b9435a0a4e5
Anoncyn <quantum3556@gmail.com>
2024-11-04 13:51:04 -0600

    feat: streamline setup script with user prompts, multilib support, and bootloader options

--- install-2.sh
+++ install-2.sh
@@ -1,73 +1,70 @@
 #!/bin/bash
 
-# Prompt the user for their desired username and hostname
+# Prompt the user for their desired username, hostname, and disk name
 read -p "Enter your username: " username
 read -p "Enter your hostname: " hostname
-read -p "Enter your disk name again (e.g. sda): " ddisk
+read -p "Enter your disk name (e.g., sda): " ddisk
 
-# Set timezone and system clock
+# Set timezone and sync hardware clock
 ln -sf /usr/share/zoneinfo/America/Mexico_City /etc/localtime
 hwclock --systohc
 
-# Install necessary packages
-pacman -S --noconfirm nano bash-completion git grub
+# Install necessary packages in a single command
+pacman -S --noconfirm nano bash-completion git grub networkmanager
 
-# Set system locale
+# Enable multilib repository in pacman configuration
+sed -i '/\[multilib\]/,/Include/s/^#//' /etc/pacman.conf
+
+# Configure system locale
 sed -i '/en_US.UTF-8/s/^#//g' /etc/locale.gen
-echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 locale-gen
 
-# Set user-defined hostname
+# Set hostname
 echo "$hostname" > /etc/hostname
 
-# Set password for root
-echo "Enter your 'root' password"
+# Set root password
+echo "Enter root password"
 passwd
 
-# Create user with specified username and add to necessary groups
+# Create user and set password
 useradd -m -g users -G wheel,storage,power -s /bin/bash "$username"
-echo "Enter your password for $username"
+echo "Enter password for user '$username'"
 passwd "$username"
 
-# Configure sudoers file
-EDITOR=nano visudo
+# Configure sudoers for wheel group with root password requirement
+echo "Defaults rootpw" | EDITOR='tee -a' visudo > /dev/null
+echo "%wheel ALL=(ALL) ALL" | EDITOR='tee -a' visudo > /dev/null
 
-# Additional configurations (if needed)
-nano /etc/pacman.conf
-
-# Install bootloader
-bootctl install
-
-# Create bootloader entries
-mkdir -p /boot/loader/entries
-echo "title Arch Linux" > /boot/loader/entries/arch.conf
-echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
-echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
-echo "options root=PARTUUID=$(blkid -s PARTUUID -o value /dev/${ddisk}3) rw" >> /boot/loader/entries/arch.conf
-
-# Ask user if they want to install GRUB with the Catppuccin theme
-read -p "Do you want to install GRUB with the Catppuccin theme? (y/n): " install_grub
+# Ask if user wants to install GRUB with the Catppuccin theme
+read -p "Install GRUB with Catppuccin theme? (y/n): " install_grub
 
 if [[ "$install_grub" == "y" ]]; then
-    # Install GRUB on the specified disk
+    # Install and configure GRUB
     grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-
-    # Configure GRUB
     grub-mkconfig -o /boot/grub/grub.cfg
 
-    # Download and apply the Catppuccin theme (assuming a Catppuccin GRUB theme repository exists)
+    # Apply the Catppuccin theme
     git clone https://github.com/catppuccin/grub.git
-    sudo cp -r grub/src/* /usr/share/grub/themes/
+    cp -r grub/src/* /usr/share/grub/themes/
     echo 'GRUB_THEME="/usr/share/grub/themes/catppuccin-mocha-grub-theme/theme.txt"' >> /etc/default/grub
     grub-mkconfig -o /boot/grub/grub.cfg
-    echo "GRUB with the Catppuccin theme has been installed."
+    echo "GRUB with the Catppuccin theme installed."
+else
+    # Install systemd-boot if GRUB is not selected
+    bootctl install
+
+    # Create bootloader entry for systemd-boot
+    cat <<EOF > /boot/loader/entries/arch.conf
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=PARTUUID=$(blkid -s PARTUUID -o value /dev/${ddisk}3) rw
+EOF
 fi
 
-# Install and enable NetworkManager
-pacman -Sy --noconfirm
-pacman -S --noconfirm networkmanager
+# Enable NetworkManager service
 systemctl enable NetworkManager.service
 
 # Completion message
-echo "Done! Press Ctrl-D to exit the chroot environment, then run 'umount -R /mnt;reboot'."
-exit
+echo "Setup complete! Press Ctrl+D to exit chroot, then run 'umount -R /mnt;reboot'."
---
+++ readme.md
@@ -0,0 +1,9 @@
+This script installs Arch Linux successfully by using two scripts.
+
+# Usage:
+
+## Run `bash <(https://raw.githubusercontent.com/undefinedcode0/install/main/install.sh)`
+-# First time (not in chroot)
+
+## Then run `bash <(https://raw.githubusercontent/undefinedcode0/install/main/install-2.sh)`
+-# Second time (in chroot)
